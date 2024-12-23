59719579f0c6d792b7a21fad6b254d1cad68163b
B.AMIT KUMAR PATRO <77608956+Bamit-2021@users.noreply.github.com>
2024-12-23 16:11:28 +0530

    Customer.py

--- Customer.py
+++ Customer.py
@@ -1,1 +1,21 @@
+def execute_script():
 
+#Sample logic: Adding two numbers
+
+  num1 = 10
+
+  num2 = 20
+
+#Perform the addition
+
+  result = num1 + num2
+
+#Print the resulta
+
+  print(f"The sum of {num1} and {num2) is {result}")
+
+# Ensure that the script is executed when the file is run
+
+if __name == "__main__":
+
+  execute_script()
